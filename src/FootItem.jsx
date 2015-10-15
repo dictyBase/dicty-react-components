4ad2aa4aae72133b8a614345cb1ed349f7a1f8ce
Haridu <haridu.naveen@gmail.com>
2015-10-15 11:42:15 -0500

    adding dicty-footer react components

---
+++ src/FootItem.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default class FootItem extends React.Component {
+    displayName = 'Footer component for easy navigation between pages'
+
+    static propTypes = {
+       title: React.PropTypes.string,
+       menuItems: React.PropTypes.array
+    }
+
+    render() {
+        const {title, menuItems} = this.props;
+        return (
+            <div className="col-lg-2 col-md-2 col-sm-4 col-xs-12">
+                <h4 className="item-header"> <a href="#">{title}</a></h4>
+                    <ul className="item-links">
+                    {
+                        menuItems.map(item => {
+                            return (
+                                <li key={menuItems.indexOf(item)}>
+                                    <a href={item.href}>{item.name}</a>
+                                </li>
+                            );
+                        })
+                    }
+                    </ul>
+            </div>
+        );
+    }
+}
