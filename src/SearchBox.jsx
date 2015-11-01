d21ef2ac5fd8ece916cdfee0c3ba2fc6654d4689
Haridu <haridu.naveen@gmail.com>
2015-11-01 15:27:10 -0600

    importing bootstrap

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,4 +1,5 @@
 import './styles/flexboxgrid';
+import './styles/bootstrap';
 import React from 'react';
 
 export default class SearchBox extends React.Component {
@@ -7,12 +8,12 @@ export default class SearchBox extends React.Component {
     render() {
         return (
             <div className="row end-md">
-                <div className="col-md-12">
+                <div className="col-md-6">
                     <div className="input-group">
                         <input type="text" className="form-control" placeholder="Guided Search" />
                         <span className="input-group-btn">
                             <button className="btn btn-default" type="button">
-                                <span className="glyphicon glyphicon-search"></span>
+                                <span className="fa fa-search"></span>
                             </button>
                         </span>
                     </div>
