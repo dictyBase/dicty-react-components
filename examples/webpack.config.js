93cc515fe41d39c72fa3a058ac8d59e5c850492a
Haridu <haridu.naveen@gmail.com>
2015-11-02 15:26:52 -0600

    added url loader & file loader

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -44,7 +44,16 @@ module.exports = {
           test: /\.css$/,
           include: path.join(__dirname, '../src/styles/'),
           loader: 'style!css'
-        }
-    ]
+        },
+        {
+          test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          include: path.join(__dirname, '../src/'),
+          loader: 'url-loader'
+        },
+        {
+          test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          include: path.join(__dirname, '../src/'),
+          loader: 'file-loader'
+        }]
   }
 };
