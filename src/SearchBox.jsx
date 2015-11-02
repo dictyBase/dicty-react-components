9f19e5a4e8da7b1960ecc59dce7697a513146385
Haridu <haridu.naveen@gmail.com>
2015-11-02 15:36:11 -0600

    added props

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,19 +1,32 @@
 import './styles/flexboxgrid';
 import './styles/bootstrap';
+import './styles/font-awesome';
 import React from 'react';
 
 export default class SearchBox extends React.Component {
     displayName = 'Search box component'
 
+    static propTypes = {
+        btnStyle: React.PropTypes.string,
+        btnIcon: React.PropTypes.string,
+        placeHolder: React.PropTypes.string
+    }
+    static defaultProps = {
+        btnStyle: 'btn btn-default',
+        btnIcon: 'fa fa-search',
+        placeHolder: 'Guided Search'
+    }
+
     render() {
+        const {btnStyle, btnIcon, placeHolder} = this.props;
         return (
             <div className="row end-md">
                 <div className="col-md-6">
                     <div className="input-group">
-                        <input type="text" className="form-control" placeholder="Guided Search" />
+                        <input type="text" className="form-control" placeholder={placeHolder} />
                         <span className="input-group-btn">
-                            <button className="btn btn-default" type="button">
-                                <span className="fa fa-search"></span>
+                            <button className={btnStyle} type="button">
+                                <span className={btnIcon}></span>
                             </button>
                         </span>
                     </div>
