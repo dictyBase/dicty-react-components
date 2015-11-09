08ec3afdb67fca1fabe772539be477a3b7951eb5
Haridu <haridu.naveen@gmail.com>
2015-11-09 15:58:30 -0600

    update
    added props, class name change, and updated responsive view

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,18 +2,23 @@ import './styles/flexboxgrid';
 import './styles/dicty-footer';
 import React from 'react';
 
-export default class DictyFooter extends React.Component {
+export default class Footer extends React.Component {
     displayName = 'A top level component for footer'
 
     static propTypes = {
-        footerStyle: React.PropTypes.object
+        footerStyle: React.PropTypes.object,
+        containerFluid: React.PropTypes.bool
+    }
+    static defaultProps = {
+        containerFluid: true
     }
 
     render() {
+        const {footerStyle, containerFluid} = this.props;
         return (
-            <div className="footer" style={this.props.footerStyle}>
-                <div className="container-fluid" >
-                    <div className="row center-xs" >
+            <div className="footer" style={footerStyle} >
+                <div className={containerFluid ? 'container-fluid' : 'container'} >
+                    <div className="row center-xs start-sm around-sm" >
                         {this.props.children}
                     </div>
                 </div>
