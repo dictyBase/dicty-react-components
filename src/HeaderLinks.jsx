8f7e76a479d38a7311f7e2a6665cf008043a494a
Haridu <haridu.naveen@gmail.com>
2016-07-05 13:43:34 -0500

    added inline styles

--- src/HeaderLinks.jsx
+++ src/HeaderLinks.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Radium from 'radium'
 import Row from './layout/row'
 import Column from './layout/column'
-import './styles/dicty-header'
 import 'font-awesome/css/font-awesome.css'
 
+@Radium
 export default class HeaderLinks extends React.Component {
     displayName = 'A component to display a list of links in the header'
 
@@ -11,24 +12,52 @@ export default class HeaderLinks extends React.Component {
         links: React.PropTypes.array,
         style: React.PropTypes.object
     }
-
-    render() {
-        const {links, style} = this.props
-        const rowStyle = {
-            textAlign: 'center',
-            '@media (min-width: 768px)': {
-                textAlign: 'end'
+    getStyles = () => {
+        return {
+            base: {
+                listStyle: 'none',
+                font: '12px helvetica,arial,sans-serif',
+                '@media (max-width : 767px)': {
+                    marginTop: '25px',
+                    marginBottom: '20px'
+                }
+            },
+            list: {
+                display: 'inline'
+            },
+            link: {
+                textDecoration: 'none',
+                color: '#1F4484',
+                padding: '8px',
+                ':active': {
+                    color: '#1F4484'
+                },
+                ':hover': {
+                    color: '#3498db'
+                }
+            },
+            row: {
+                textAlign: 'center',
+                '@media (min-width: 768px)': {
+                    textAlign: 'end'
+                }
             }
         }
+    }
+    render() {
+        const {links, style} = this.props
+        const defStyle = this.getStyles()
         return (
-            <Row style={ rowStyle }>
+            <Row style={ defStyle.row }>
                 <Column
                   xsSpan={ 12 }>
-                    <ul className="header-links">
+                    <ul style={ defStyle.base }>
                         { links.map((link, index) => {
                             return (
-                                <li key={ index }>
-                                    <a href={ link.href } style={ style }>
+                                <li key={ index } style={ defStyle.list }>
+                                    <a key={ index }
+                                      href={ link.href }
+                                      style={ [defStyle.link, style && style] }>
                                         <span className={ link.iconClass }></span>
                                         { ' ' }{ link.name }
                                     </a>
