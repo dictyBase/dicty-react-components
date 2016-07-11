d49347fdf29e0f28ec26f31ce57c8bb26ed03e31
Haridu <haridu.naveen@gmail.com>
2016-07-10 21:31:43 -0500

    using radium grid

--- examples/header/app.jsx
+++ examples/header/app.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { render } from 'react-dom'
 import Header from '../../src/Header'
 import HeaderLogo from '../../src/HeaderLogo'
-import HeaderItems from '../../src/HeaderItems'
 import HeaderLinks from '../../src/HeaderLinks'
 import SearchBox from '../../src/SearchBox'
 import { StyleRoot } from 'radium'
+import { Grid, Cell } from 'radium-grid'
 
 
 class DictyHeader extends React.Component {
@@ -19,11 +19,24 @@ class DictyHeader extends React.Component {
         return (
             <StyleRoot>
                 <Header>
-                    <HeaderLogo path= "../src/img/dicty-logo.png" href= "http://dictybase.org/"/>
-                    <HeaderItems>
-                        <HeaderLinks links= { headerLinks } />
-                        <SearchBox />
-                    </HeaderItems>
+                    <Grid smallCellWidth="1" mediumCellWidth="1">
+                        <Cell smallAlign="center" mediumAlign="center">
+                            <HeaderLogo
+                              path= "../src/img/dicty-logo.png"
+                              href= "http://dictybase.org/"
+                            />
+                        </Cell>
+                        <Cell>
+                            <Grid cellWidth="1">
+                                <Cell align="right" smallAlign="center" mediumAlign="center">
+                                    <HeaderLinks links= { headerLinks } />
+                                </Cell>
+                                <Cell>
+                                    <SearchBox />
+                                </Cell>
+                            </Grid>
+                        </Cell>
+                    </Grid>
                 </Header>
             </StyleRoot>
         )
